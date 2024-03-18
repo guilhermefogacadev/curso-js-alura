20fb6605e56001a93aa656106c01c901163d1133
Guilherme Fogaça <guilhermefogacamessias@alunos.utfpr.edu.br>
2024-03-18 14:44:01 -0300

    add responsive voice

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function aleatorio() {
 function exibirNaTela(tag, texto) {
     let campo = document.querySelector(tag)
     campo.innerHTML = texto
+    responsiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1.2});
 }
 
 function mensagemIncial(){
